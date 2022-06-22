ee53c1a8fa0d840d95f28a8bb9fe551954beb151
arturmingaleev <art.mingaleev@yandex.ru>
2022-06-22 23:36:52 +0300

    Урок 1 | Добавил кнопку в активити и повесил на нее ClickListener с логом

--- app/src/main/java/ru/mingaleev/androidkotlin/MainActivity.kt
+++ app/src/main/java/ru/mingaleev/androidkotlin/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.mingaleev.androidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<AppCompatButton>(R.id.btn).setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Log.d("Test", "Нажата кнопка")
+            }
+
+        })
     }
 }
