59271305af8c17bb2af6f20c69784f49dd2d3809
arturmingaleev <art.mingaleev@yandex.ru>
2022-06-24 11:36:27 +0300

    Урок 1 | Добавил when и вывел в лог

--- app/src/main/java/ru/mingaleev/androidkotlin/MainActivity.kt
+++ app/src/main/java/ru/mingaleev/androidkotlin/MainActivity.kt
@@ -18,12 +18,23 @@ class MainActivity : AppCompatActivity() {
             secondDT.clicking()
             Log.d("Test", "${secondDT.name} нажал кнопку ${secondDT.counter.toString()}й раз")
             Log.d("Test", "Результат ${comparison(secondDT.counter)}")
+            Log.d("Test --5--", even(secondDT.counter))
 
         }
     }
 
     private fun comparison (counter: Int): String {
         return if (counter >= 20) "20 раз или больше" else "меньше 20 раз"
+    }
+
+    private fun even (counter: Int): String{
+        return when (counter) {
+            in 1..4 -> "пока мало"
+            5 -> "5 раз!"
+            in 6..9 -> "уже нормально"
+            10 -> "уже 10 раз!"
+            else -> "многовато"
+        }
     }
 
     data class firstDataClass (val name: String, var counter: Int) {
