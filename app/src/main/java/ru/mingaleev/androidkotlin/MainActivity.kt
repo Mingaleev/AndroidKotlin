331f9c9499fea46ec97bf40f46a3fc00c3cfd186
arturmingaleev <art.mingaleev@yandex.ru>
2022-06-24 09:36:38 +0300

    Урок 1 | Добавил if и вывел результат в лог

--- app/src/main/java/ru/mingaleev/androidkotlin/MainActivity.kt
+++ app/src/main/java/ru/mingaleev/androidkotlin/MainActivity.kt
@@ -12,11 +12,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val firstDT = firstDataClass ("Minga", 0)
-        val secondDT = firstDT.copy("Artur", 1)
+        val secondDT = firstDT.copy(name = "Artur", counter = 1)
 
         findViewById<AppCompatButton>(R.id.btn).setOnClickListener {
             secondDT.clicking()
             Log.d("Test", "${secondDT.name} нажал кнопку ${secondDT.counter.toString()}й раз")
+            Log.d("Test", "Результат ${comparison(secondDT.counter)}")
+
+        }
+    }
+
+    private fun comparison (counter: Int): String {
+        return if (counter >= 20) {
+            "20 раз или больше"
+        } else {
+            "меньше 20 раз"
         }
     }
 
